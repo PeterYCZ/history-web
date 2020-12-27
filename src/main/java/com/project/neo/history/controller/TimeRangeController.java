ea343050884059e561ba0a77612b75c95b7fc6c1
PeterYCZ <yecongzhoupeter@qq.com>
2020-12-27 21:55:02 +0800

    [修改] 时间查询修改

--- src/main/java/com/project/neo/history/controller/TimeRangeController.java
+++ src/main/java/com/project/neo/history/controller/TimeRangeController.java
@@ -1,8 +1,6 @@
 package com.project.neo.history.controller;
 
-import com.project.neo.history.entity.Event;
-import com.project.neo.history.entity.TimeQuantum;
-import com.project.neo.history.entity.TimeQuantumEvents;
+import com.project.neo.history.entity.*;
 import com.project.neo.history.service.EventRepository;
 import com.project.neo.history.service.TimeQuantumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +20,13 @@ public class TimeRangeController {
     private EventRepository eventRepository;
 
     @GetMapping("/api/v1/getTimeRange/{name}")
-    public TimeQuantumEvents getTimeQuantumByName(@PathVariable String name){
+    public GraphDataVO getTimeQuantumByName(@PathVariable String name){
+        GraphDataVO graphDataVO = new GraphDataVO();
         TimeQuantum timeQuantum = timeQuantumRepository.findAllByName(name);
-        List<Event> events = eventRepository.findAllByTimeQuantumsName(name);
-        TimeQuantumEvents timeQuantums = new TimeQuantumEvents();
-        timeQuantums.setName(timeQuantum.getName());
-        timeQuantums.setStartYear(timeQuantum.getStartYear());
-        timeQuantums.setEndYear(timeQuantum.getEndYear());
-        timeQuantums.setEventList(events);
-        return timeQuantums;
+        List<Event> events = eventRepository.findAllByTimeQuantumsName(timeQuantum.getName());
+        graphDataVO.addData(timeQuantum);
+        GraphDataVO.addFromEvent(graphDataVO,events);
+        return graphDataVO;
     }
 
     @GetMapping("/api/v1/getYear/{year}")
